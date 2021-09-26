fb4f5353c3b81241974480e385498d8b7fc6833a
Johannes Konings <johannes.konings@outlook.com>
2021-09-26 20:04:42 +0200

    add kms key and dynamodb table

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,25 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as kms from "@aws-cdk/aws-kms";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const name = `persons-cdk`;
+
+    const kmsKey = new kms.Key(this, "kmsKey", {
+      enableKeyRotation: true,
+    });
+
+    kmsKey.addAlias(name);
+
+    const table = new dynamodb.Table(this, "Table", {
+      tableName: name,
+      partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
+      encryptionKey: kmsKey,
+    });
   }
 }
