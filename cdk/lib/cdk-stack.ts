354b56e4f62642c6c65e9f967392eb5ddc3a4639
Johannes Konings <johannes.konings@outlook.com>
2021-09-26 20:32:35 +0200

    add kinesis strem

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as kms from "@aws-cdk/aws-kms";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as kinesis from "@aws-cdk/aws-kinesis";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -14,12 +15,19 @@ export class CdkStack extends cdk.Stack {
 
     kmsKey.addAlias(name);
 
+    const stream = new kinesis.Stream(this, "Stream", {
+      streamName: `${name}-data-stream`,
+      encryption: kinesis.StreamEncryption.KMS,
+      encryptionKey: kmsKey,
+    });
+
     const table = new dynamodb.Table(this, "Table", {
       tableName: name,
       partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
       encryptionKey: kmsKey,
+      kinesisStream: stream,
     });
   }
 }
