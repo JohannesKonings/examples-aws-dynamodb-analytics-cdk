eb2a34e28539a6f568f153e634213828e07741d7
Johannes Konings <johannes.konings@outlook.com>
2021-09-27 19:43:18 +0200

    add crawler

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,6 +5,8 @@ import * as kinesis from "@aws-cdk/aws-kinesis";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as destinations from "@aws-cdk/aws-kinesisfirehose-destinations";
 import * as firehose from "@aws-cdk/aws-kinesisfirehose";
+import * as glue from "@aws-cdk/aws-glue";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -51,6 +53,30 @@ export class CdkStack extends cdk.Stack {
     const athenaQueryResults = new s3.Bucket(this, "query-results", {
       bucketName: `${name}-query-results`,
       encryptionKey: kmsKey,
+    });
+
+    const roleCrawler = new iam.Role(this, "role crawler", {
+      assumedBy: new iam.ServicePrincipal("glue.amazonaws.com"),
+    });
+
+    roleCrawler.addToPolicy(
+      new iam.PolicyStatement({
+        resources: ["*"],
+        actions: ["*"],
+      })
+    );
+
+    const crawler = new glue.CfnCrawler(this, "crawler", {
+      name: `${name}-crawler`,
+      role: roleCrawler.roleArn,
+      targets: {
+        s3Targets: [
+          {
+            path: `s3://${firehoseBucket.bucketName}`,
+          },
+        ],
+      },
+      databaseName: `${name}-db`,
     });
   }
 }
